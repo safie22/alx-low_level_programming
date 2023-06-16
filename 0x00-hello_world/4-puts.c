ed1029753d69cceddd501e8c0d007f6bae998c0e
safie22 <safie20042210@gmail.com>
2023-06-15 17:13:03 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main ~ Enter point
+ * 
+ * Description: A C program that prints whih put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)  
+{	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
